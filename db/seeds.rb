d90607150d4f28e198c04afa1c301f9169ec5c92
alexandre valentin <alexandre.valentin42@gmail.com>
2024-11-14 11:03:26 +0100

    Exercice rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "On nettoie la Seed"
+Restaurant.destroy_all
+puts "On creer de nouveaux restaurants"
+Restaurant.create!(name:"Hoppstore", address:"Place Sathonay", phone_number: "06 72 39 45 21", category: "belgian")
+Restaurant.create!(name:"Paddys", address:"Place des Tapis", phone_number: "06 23 39 90 21", category: "belgian")
+Restaurant.create!(name:"Bouchon", address:"Vieux Lyon", phone_number: "06=4 23 56 99 22", category: "french")
+Restaurant.create!(name:"Vitto", address:"Rue de Rome", phone_number: "07 21 34 90 21", category: "italian")
+Restaurant.create!(name:"Le soleil levant", address:"Place de Tokyo", phone_number: "06 23 23 23 21", category: "japenese")
+puts "Seeds créés"
